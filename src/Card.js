f56936513f0000e79dcb25735e9af31fcc2879e8
tan27 <tn2706@gmail.com>
2021-05-28 16:00:04 -0700

    add id

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Card({name}) {
+function Card({name, id}) {
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img src="https://robohash.org/test?200x200" alt="logo" />
+        <img src={`https://robohash.org/${id}?200x200`} alt="logo" />
         <div>
           <h1>{name}</h1>
         </div>
--- src/CardList.js
+++ src/CardList.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import Card from './Card'
 
-function CardList ( { robots } ) {
-    const cardArray = robots.map((user, i) => {
-        return (
-            <Card name={robots[i].name}/>
-        )
-    })
-
+const CardList = ({ robots }) => {
     return (
         <div>
-        { cardArray }
+            {
+            robots.map((user, i) => {
+            return (
+                <Card
+                name={robots[i].name}
+                id={robots[i].id}
+
+                /> 
+                );
+            })
+            }
         </div>   
-    )
+    );
 }
 
 export default CardList;
