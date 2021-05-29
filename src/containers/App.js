ee4cce7ff40a2e049c85bf8e35fb788312678d20
tan27 <tn2706@gmail.com>
2021-05-29 00:33:56 -0700

    finalizing

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,12 +21,6 @@ class App extends Component {
         })
     }
 
-    // async function() {
-    //     const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    //     const users = await response.json()
-    //     return (users => {this.setState({robots: users})
-    //     }
-    // }
 
     onSearchChange = (e) => {
         this.setState({searchfield: e.target.value})
@@ -34,13 +28,13 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots= this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield} = this.state;
+        const filteredRobots= robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-        return (
+        return (robots.length === 0) ? 
+            <h1>Loading</h1> : 
+            (
             <>
             <h1 className='tc f2'>Robots</h1>
             <Searchbox searchChange = {this.onSearchChange} />
@@ -49,7 +43,6 @@ class App extends Component {
             </Scroll>
             </>
         );
-        }
     }
 }
 
